c1cbc770dfdfd0c1b779b2d2a5a4329323adde13
mdzxtmt <2517294759@qq.com>
2026-09-13 13:16:31 +0800

    update

--- components/file/index.ts
+++
@@ -1,8 +0,0 @@
-// 文件相关组件导出
-export { default as FileUpload } from './FileUpload'
-export { default as UploadProgress, UploadQueue } from './UploadProgress'
-export { default as UploadManager } from './UploadManager'
-export { default as FileCard } from './FileCard'
-export { default as FileList } from './FileList'
-export { default as FilePreview } from './FilePreview'
-export { default as FileSearch } from './FileSearch'
